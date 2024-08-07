f2d34f87444a8e404ad8d0093871eba98082abbd
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-08-07 16:27:31 +1000

    benchmarks

---
+++ AutoMapperBlog/Benchmarks/Program.cs
@@ -0,0 +1,43 @@
+﻿using App.Manual;
+using AutoMapper;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Domain;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+public class Benchmarks
+{
+    private static readonly WeatherForecast DomainEntity = new WeatherForecast
+    {
+        Date = new DateOnly(2021, 1, 1),
+        TemperatureC = 25,
+        Summary = "Mild"
+    };
+    
+    [Benchmark(Baseline = true)]
+    public int Manual()
+    {
+        var dto = DomainEntity.ToDto();
+
+        return dto.TemperatureF;
+    }
+    
+    private static readonly IMapper Mapper = new MapperConfiguration(cfg => cfg.AddProfile<App.AutoMapper.Mapping>()).CreateMapper();
+    
+    [Benchmark]
+    public int AutoMapper()
+    {
+        var dto = Mapper.Map<App.AutoMapper.WeatherForecastDto>(DomainEntity);
+
+        return dto.TemperatureF;
+    }
+    
+    [Benchmark]
+    public int Mapperly()
+    {
+        var dto = App.Mapperly.Mapper.ToDto(DomainEntity);
+
+        return dto.TemperatureF;
+    }
+}
